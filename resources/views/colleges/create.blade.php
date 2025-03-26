3a95cfc533d7dd5511dc4cb3020f6e8e650e0d8d
Palacat Rachel Marie <palcatmarie@gmail.com>
2025-03-26 14:19:39 +0800

    minor changes in html

--- resources/views/colleges/create.blade.php
+++ resources/views/colleges/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-4">
     <div class="card shadow-lg">
         <div class="card-header bg-primary text-white">
-            <h4 class="mb-0">Add New College</h4>
+            <h4 class="mb-0">Add New Colleges</h4>
         </div>
         <div class="card-body">
             <form action="{{ route('colleges.store') }}" method="POST">
